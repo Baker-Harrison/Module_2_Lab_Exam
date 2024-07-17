b565047d7f678ccbb35bd915a3f1680d2caa5b2d
BachiBachBach <thehessboi@gmail.com>
2024-07-17 10:35:31 -0500

    added second date example

--- exam2.cpp
+++ exam2.cpp
@@ -5,10 +5,16 @@
 int main() {
 	Date date;
 
+	Date date2 (5, 8, 2006);
+
 	date.print1();
 	date.print2();
 	date.print3();
 
+	cout  << "-----------------------" << endl;
+	date2.print1();
+	date2.print2();
+	date2.print3();
 
 
 	return 0;
